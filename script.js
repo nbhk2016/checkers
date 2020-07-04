d1e04b776422fca735861fb2680b18343940463b
xiirx <nbhk2016@ya.ru>
2020-07-04 18:06:58 +0300

    шашки ходят коректно

--- script.js
+++ script.js
@@ -10,28 +10,67 @@ class Figure {
 		this.color = color
 	}
 
-	goTo(x, y){
-		if (Math.abs(this.x - x) == 1 && (this.color == 'black' && this.y - y == -1 || this.color == 'white' && this.y - y == 1) && (this.x != x && this.y != y) && (!field[x][y].figure) && field[x][y].color == 'black') {
-			document.querySelectorAll(`td[data-x="${this.x}"][data-y="${this.y}"]>span`)[0].remove()
-			delete field[this.x][this.y].figure
-			let elem = document.createElement('span')
-			if (this.color == 'white'){
-				elem.classList.add('whiteFigure')
-				elem.setAttribute('data-color', 'white')
-			}
-
-			else {
-				elem.classList.add('blackFigure')
-				elem.setAttribute('data-color', 'black')
-			}
-
-			document.querySelectorAll(`td[data-x="${x}"][data-y="${y}"]`)[0].appendChild(elem)
-
-			this.x = x
-			this.y = y
-
-			field[x][y].figure = this
+	move(x, y){
+		document.querySelectorAll(`td[data-x="${this.x}"][data-y="${this.y}"]>span`)[0].remove()
+		delete field[this.x][this.y].figure
+		let elem = document.createElement('span')
+		if (this.color == 'white'){
+			elem.classList.add('whiteFigure')
+			elem.setAttribute('data-color', 'white')
 		}
+
+		else {
+			elem.classList.add('blackFigure')
+			elem.setAttribute('data-color', 'black')
+		}
+
+		document.querySelectorAll(`td[data-x="${x}"][data-y="${y}"]`)[0].appendChild(elem)
+
+		this.x = x
+		this.y = y
+
+		field[x][y].figure = this
+	}
+
+	goTo(x, y){
+		if ((this.x != x && this.y != y) && (!field[x][y].figure) && field[x][y].color == 'black') {
+			if (Math.abs(this.x - x) == 1 && (this.color == 'black' && this.y - y == -1 || this.color == 'white' && this.y - y == 1))
+				this.move(x, y)
+			 
+			else if (Math.abs(this.x - x) == 2 && (this.color == 'black' && this.y - y == -2 || this.color == 'white' && this.y - y == 2)){
+				if (this.color == 'white'){
+					if (this.x - x == 2 && field[this.x - 1][this.y - 1].figure && field[x + 1][y + 1].figure && field[x + 1][y + 1].figure.color == 'black' && field[this.x - 1][this.y - 1].figure == field[x + 1][y + 1].figure){
+						this.move(x, y)
+						field[x + 1][y + 1].figure.del()
+					}
+
+					else if (this.x - x == -2 && field[this.x + 1][this.y - 1].figure && field[x - 1][y + 1].figure && field[x - 1][y + 1].figure.color == 'black' && field[this.x + 1][this.y - 1].figure == field[x - 1][y + 1].figure){
+						this.move(x, y)
+						field[x - 1][y + 1].figure.del()
+					}
+				} 
+
+				else if (this.color == 'black'){
+					if (this.x - x == -2 && field[x - 1][y - 1].figure && field[this.x + 1][this.y + 1].figure && field[x - 1][y - 1].figure == field[this.x + 1][this.y + 1].figure && field[this.x + 1][this.y + 1].figure.color == 'white'){
+						this.move(x, y)
+						field[x - 1][y - 1].figure.del()
+					}
+
+					else if (this.x - x == 2 && field[x + 1][y - 1].figure && field[this.x - 1][this.y + 1].figure && field[x + 1][y - 1].figure == field[this.x - 1][this.y + 1].figure && field[this.x - 1][this.y + 1].figure.color == 'white'){
+						this.move(x, y)
+						field[x + 1][y - 1].figure.del()
+					}
+
+				}
+			}
+		}
+
+		
+	}
+
+	del(){
+		delete field[this.x][this.y].figure
+		document.querySelectorAll(`td[data-x="${this.x}"][data-y="${this.y}"]>span`)[0].remove()
 	}
 }
 
@@ -48,8 +87,8 @@ class Cell {
 let tds = document.getElementsByTagName('td')
 
 for (const item of tds){
-	let x = item.getAttribute('data-x')
-	let y = item.getAttribute('data-y')
+	let x = Number(item.getAttribute('data-x'))
+	let y = Number(item.getAttribute('data-y'))
 	let color = item.classList[0]
 
 	let figColor = ''
@@ -68,8 +107,8 @@ let coords = []
 
 for (const item of tds)
 	item.addEventListener('click', (event) => {
-		let x = event.currentTarget.getAttribute('data-x')
-		let y = event.currentTarget.getAttribute('data-y')
+		let x = Number(event.currentTarget.getAttribute('data-x'))
+		let y = Number(event.currentTarget.getAttribute('data-y'))
 
 		if (counter == 0 && field[x][y].figure){
 			counter = 1
@@ -81,4 +120,3 @@ for (const item of tds)
 			counter = 0
 		}
 	})
-
